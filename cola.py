0ce1940020df9c961d263aaa3195e971758b4e24
santiagoRojas <santi_rojas123@hotmail.com>
2018-08-16 22:59:45 -0500

    programa funcionando pero sin utilizar clases del profe

---
+++ cola.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+class Cola:
+    """ Representa una cola con operaciones de encolar, desencolar y
+        verificar si está vacía. """
+
+    def __init__(self):
+        """ Crea una cola vacía. """
+        # La cola vacía se representa con una lista vacía
+        self.items=[]
+
+    def encolar(self, x):
+        """ Agrega el elemento x a la cola. """
+        # Encolar es agregar al final de la cola.
+        self.items.append(x)
+
+    def desencolar(self):
+        """ Devuelve el elemento inicial y lo elimina de la cola.
+            Si la cola está vacía levanta una excepción. """
+        try:
+            return self.items.pop(0)
+        except IndexError:
+            raise ValueError("La cola está vacía")
+
+    def es_vacia(self):
+        """ Devuelve True si la lista está vacía, False si no. """
+        return self.items == []
+
+class Estudiante:
+    def __init__(self,nombre,codigo,placa):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.placa = placa
+
+validacion = True
+estudiantes = []
+while validacion ==True:
+
+    print "bienvenidos al sistema de asignacion de cupos de parqueadero de la universidad distrital \n recuerde que los cupos son limitados\n"
+
+    nombre = raw_input("digite su nombre completo \n")
+    codigo = int(raw_input("digite su codigo de estudiante \n"))
+    placa = raw_input("digite la placa del vehiculo\n")
+
+    estudiante = Estudiante(nombre,codigo,placa)
+    estudiantes.append(estudiante)
+
+    opcion = raw_input("si desea seguir ingresando estudiantes escriba cualquier tecla, de lo contrario escriba 1 para proceguir con la asignacion de cupos")
+    if opcion == '1':
+        validacion = False
+cupos = int(input("cuantos cupos desea habilitar"))
+print "los siguientes estudiantes obtendran un cupo"
+for i in range(0,cupos):
+    try:
+        estudianteActual = estudiantes.pop(0)
+        print "estudiante numero ",i+1,"\n nombre: ",estudianteActual.nombre,"\n codigo: ",estudianteActual.codigo,"\n placa del vehiculo: ",estudianteActual.placa
+    except IndexError:
+        print "demasiados cupos, cola vacia"
---
+++ pila.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+class Pila:
+    """ Representa una pila con operaciones de apilar, desapilar y
+        verificar si está vacía. """
+
+    def __init__(self,libros):
+        """ Crea una pila vacía. """
+        # La pila vacía se r{epresenta con una lista vacía
+        self.items=libros
+
+    def apilar(self, x):
+        """ Agrega el elemento x a la pila. """
+        # Apilar es agregar al final de la lista.
+        self.items.append(x)
+
+    def desapilar(self):
+        """ Devuelve el elemento tope y lo elimina de la pila.
+            Si la pila está vacía levanta una excepción. """
+        try:
+            return self.items.pop()
+        except IndexError:
+            raise ValueError("La pila está vacía")
+
+    def es_vacia(self):
+        """ Devuelve True si la lista está vacía, False si no. """
+        return self.items == []
+class Libro:
+    def __init__(self,nombre,categoria,autor):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.autor = autor
+    def getNombre(self):
+        return self.nombre
+
+archivo = open("libros.csv","r")
+lista = [(x.split(";")[0],x.split(";")[1],x.split(";")[2]) for x in archivo.readlines()]
+
+libros = []
+
+for x in lista:
+    libro = Libro(x[0],x[1],x[2])
+    libros.append(libro)
+
+opcion = int(input("digite la opcion que desea realizar \n 1 si desea buscar por nombre del libro \n 2 si desea buscar por categoria del libro\n 3 si desea buscar por autor del libro\n"))
+if(opcion == 1 or opcion == 2 or opcion == 3):
+    busqueda = raw_input("digite para empezar la busqueda\n")
+    if opcion == 3:
+        busqueda = busqueda +" \r\n"
+    validacion = False
+    while validacion == False:
+        try:
+            ultimoLibro = libros.pop()
+            if(opcion == 1):
+                if(ultimoLibro.nombre == busqueda):
+                    validacion = True
+            elif (opcion == 2):
+                if(ultimoLibro.categoria == busqueda):
+                    validacion = True
+            elif (opcion == 3):
+                if(ultimoLibro.autor == busqueda+"\n"):
+                    print "ENTRO"
+                    validacion = True
+            if(validacion == True):
+                print "Primer libro encontrado con la especificacion es \n nombre del libro:", ultimoLibro.nombre," \n categoria: ",ultimoLibro.categoria ,"\n autor:", ultimoLibro.autor
+        except IndexError:
+            print "La pila está vacía, no se encontro un libro con estas especificaciones"
+            validacion = True
+
+else:
+    print "opcion invalida"
